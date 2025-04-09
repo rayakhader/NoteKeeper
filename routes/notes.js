afcab436ed1632252a9147d37bf4b24ce6e6d8f5
rayakhader <rayakhader20@gmail.com>
2025-04-09 23:19:58 +0200

    Add notes routes to handle CRUD API endpoints

---
+++ routes/notes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const notesController = require('../controllers/notesController');
+
+router.get('/notes', notesController.getAllNotes);
+router.post('/notes', notesController.createNote);
+router.delete('/notes/:id', notesController.deleteNote);
+router.put('/notes/:id', notesController.updateNote);
+
+module.exports = router;

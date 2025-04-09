5f78215acec1d38e5a26ea8b7cb5b0d44e4279b7
rayakhader <rayakhader20@gmail.com>
2025-04-10 01:53:48 +0200

    edit package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const notesRoutes = require('./routes/notesRoutes');
+const notesRoutes = require('./routes/notes');
 require('dotenv').config();
 const cors = require('cors');
 

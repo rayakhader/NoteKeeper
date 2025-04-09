56f74c2409b036319a96c9c251bd93f09a7820f4
rayakhader <rayakhader20@gmail.com>
2025-04-10 00:41:04 +0200

    implement notes controller with CRUD operations

---
+++ controllers/notesController.js
@@ -0,0 +1,52 @@
+const Note = require('../models/Note');
+
+exports.getAllNotes = async (req,res) =>{
+
+    try{
+       const notes =  await Note.find();
+       res.json(notes)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+exports.createNote = async (req,res) =>{
+    try{
+        const note = new Note(req.body)
+        const savedNote = await note.save()
+        res.status(201).json(savedNote)
+    }catch(error){
+        res.status(400).json({message: error.message})
+    }
+}
+exports.deleteNote = async (req, res) =>{
+    try{
+        const result = await Note.findByIdAndDelete(req.params.id);
+        if(!result) return res.status(404).json({message: 'Note not Found'})
+        res.json({message: 'Note deleted'})
+    }catch(error){
+        res.status(500).json({message : error.message})
+    }
+}
+
+exports.updateNote = async(req, res) =>{
+    try{
+        const updatedNote = await Note.findByIdAndUpdate(
+            req.params.id,
+            { title: req.body.title, content: req.body.content },
+            { new: true } // return updated doc
+          );
+        if(!result) return res.status(404).json({message: 'Note not Found'})
+        res.json(updatedNote)
+    }catch(error){
+        res.status(400).json({message : error.message})
+    }
+}
+ 
+
+
+
+
+
+
+
